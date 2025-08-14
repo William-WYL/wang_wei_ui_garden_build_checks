5fc8752c759da4fb432db50383a52617e78985bd
Wei(William) Wang <william.wang.uncharted@gmail.com>
2025-08-14 09:43:29 -0500

    Before building the portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,23 @@ import React from 'react';
 
 function App() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-      <header style={{ padding: '1rem' }}>
-        <h1>🌱 Welcome to UI Garden</h1>
-        <p>Storybook：</p>
-      </header>
+    // <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+    //   {/* <header style={{ padding: '1rem' }}>
+    //     <h1>🌱 Welcome to UI Garden</h1>
+    //     <p>Storybook：</p>
+    //   </header> */}
 
-      <iframe
-        src="/storybook/index.html"
-        title="Storybook"
-        style={{
-          flexGrow: 1,
-          border: '1px solid #ccc',
-          borderRadius: '7px',
-        }}
-      ></iframe>
-    </div>
+    //   {/* <iframe
+    //     src="/storybook/index.html"
+    //     title="Storybook"
+    //     style={{
+    //       flexGrow: 1,
+    //       border: '1px solid #ccc',
+    //       borderRadius: '7px',
+    //     }}
+    //   ></iframe> */}
+    // </div>
+    <></>
   );
 }
 
